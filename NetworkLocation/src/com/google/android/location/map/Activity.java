66c480811ff6e037ec6e8a11f5b4f8d9f14bc1a6
mar-v-in <github@marvin.awmail.org>
2012-08-02 12:06:42 +0200

    updated DebugApp

--- NetworkLocation/src/com/google/android/location/map/Activity.java
+++ NetworkLocation/src/com/google/android/location/map/Activity.java
@@ -1,15 +1,23 @@
 package com.google.android.location.map;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.telephony.CellLocation;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.android.location.DatabaseHelper;
+import com.google.android.location.GsmCellMap;
 import com.google.android.location.R;
 import com.google.android.location.WlanLocationData;
 import com.google.android.location.WlanMap;
@@ -18,7 +26,11 @@ import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 
 public class Activity extends android.app.Activity implements LocationListener {
+
+	private static final String TAG = Activity.class.getName();
 	private WlanMap wlans;
+	private GsmCellMap cells;
+	private Map<String, Location> currentWlans;
 	private DatabaseHelper helper;
 
 	private MapView mapView;
@@ -26,12 +38,55 @@ public class Activity extends android.app.Activity implements LocationListener {
 	private LocationOverlay pos_overlay;
 
 	private WlanOverlay wlan_overlay;
+	private TextView youLat;
+	private TextView youLon;
+	private TextView cellMcc;
+	private TextView cellMnc;
+	private TextView cellCid;
+	private TextView cellLat;
+	private TextView cellLon;
+	private ListView wlanList;
+	private WlanAdapter wlanAdapter;
+	private TelephonyManager telephonyManager;
+
+	private GsmCellLocation getGsmCellLocation() {
+		final CellLocation cellLocation = telephonyManager.getCellLocation();
+		if (cellLocation instanceof GsmCellLocation) {
+			return (GsmCellLocation) cellLocation;
+		}
+		return null;
+	}
 
 	private Collection<String> getWLANs() {
 		return WlanLocationData.getWLANs(this);
 	}
 
 	private void makeLocationVisible(Location loc) {
+		youLat.setText("Lat: " + loc.getLatitude());
+		youLon.setText("Lon: " + loc.getLongitude());
+		final GsmCellLocation cell = getGsmCellLocation();
+		int cid = -1, mcc = -1, mnc = -1;
+		if (cell == null) {
+			cellCid.setText("CID: ---");
+		} else {
+			cid = cell.getCid();
+			cellCid.setText("CID: " + cid);
+		}
+		final String operator = telephonyManager.getNetworkOperator();
+		if (operator == null || operator.length() <= 3) {
+			cellMcc.setText("MCC: ---");
+			cellMnc.setText("MNC: ---");
+		} else {
+			mcc = Integer.parseInt(operator.substring(0, 3));
+			mnc = Integer.parseInt(operator.substring(3));
+			cellMcc.setText("MCC: " + mcc);
+			cellMnc.setText("MNC: " + mnc);
+		}
+		if (cid != -1 && mcc != -1 && mnc != -1) {
+			final Location l = cells.get(mcc, mnc, cid);
+			cellLat.setText("Lat: " + l.getLatitude());
+			cellLon.setText("Lon: " + l.getLongitude());
+		}
 		final GeoPoint pt = new GeoPoint(loc);
 		wlan_overlay.removeAll();
 		pos_overlay.setLocation(loc);
@@ -39,18 +94,38 @@ public class Activity extends android.app.Activity implements LocationListener {
 		final Collection<String> macs = getWLANs();
 		final Map<String, Location> cache = wlans.getNext(loc.getLatitude(),
 				loc.getLongitude(), 400);
+		currentWlans.clear();
 		for (final String mac : cache.keySet()) {
-			final Location l = cache.get(mac);
-			wlan_overlay.addItem(l, macs.contains(mac), mac, null);
+			if (!macs.contains(mac)) {
+				final Location l = cache.get(mac);
+				wlan_overlay.addItem(l, false, mac, null);
+			}
 		}
-		Log.d("LocationActivity", "locked at " + pt.toDoubleString());
+		for (final String mac : macs) {
+			final Location l = wlans.get(mac);
+			wlan_overlay.addItem(l, true, mac, null);
+			currentWlans.put(mac, l);
+		}
+		wlanAdapter.notifyDataSetChanged();
+		Log.d(TAG, "locked at " + pt.toDoubleString());
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		currentWlans = new HashMap<String, Location>();
 		mapView = (MapView) findViewById(R.id.mapView);
+		youLat = (TextView) findViewById(R.id.you_lat);
+		youLon = (TextView) findViewById(R.id.you_lon);
+		cellMcc = (TextView) findViewById(R.id.mcc);
+		cellMnc = (TextView) findViewById(R.id.mnc);
+		cellCid = (TextView) findViewById(R.id.cid);
+		cellLat = (TextView) findViewById(R.id.cell_lat);
+		cellLon = (TextView) findViewById(R.id.cell_lon);
+		wlanList = (ListView) findViewById(R.id.wlan_list);
+		wlanAdapter = new WlanAdapter(this, currentWlans);
+		wlanList.setAdapter(wlanAdapter);
 		mc = mapView.getController();
 		wlan_overlay = new WlanOverlay(this);
 		pos_overlay = new LocationOverlay(this);
@@ -59,6 +134,8 @@ public class Activity extends android.app.Activity implements LocationListener {
 		mc.setZoom(17);
 		helper = new DatabaseHelper(this);
 		wlans = new WlanMap(helper);
+		cells = new GsmCellMap(helper);
+		telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
 		final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 		locationManager.requestLocationUpdates(
 				LocationManager.NETWORK_PROVIDER, 2000, 10, this);
---
+++ NetworkLocation/src/com/google/android/location/map/WlanAdapter.java
@@ -0,0 +1,59 @@
+package com.google.android.location.map;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import android.content.Context;
+import android.location.Location;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.google.android.location.R;
+
+public class WlanAdapter extends BaseAdapter {
+
+	private final Map<String, Location> wlans;
+	private final Context context;
+
+	public WlanAdapter(Context context, Map<String, Location> wlans) {
+		this.context = context;
+		this.wlans = wlans;
+	}
+
+	@Override
+	public int getCount() {
+		return wlans.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return wlans.values().toArray()[position];
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return wlans.keySet().toArray()[position].hashCode();
+	}
+
+	@Override
+	public View getView(int position, View view, ViewGroup parent) {
+		if (view == null) {
+			final LayoutInflater layoutInflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = layoutInflater.inflate(R.layout.wlan_entry, parent, false);
+		}
+		final TextView lat = (TextView) view.findViewById(R.id.wlan_lat);
+		final TextView lon = (TextView) view.findViewById(R.id.wlan_lon);
+		final TextView mac = (TextView) view.findViewById(R.id.mac);
+		final Object[] array = wlans.entrySet().toArray();
+		final Entry<String, Location> entry = (Entry<String, Location>) array[position];
+		lat.setText("Lat: " + entry.getValue().getLatitude());
+		lon.setText("Lon: " + entry.getValue().getLongitude());
+		mac.setText("MAC: " + entry.getKey());
+		return view;
+	}
+
+}
