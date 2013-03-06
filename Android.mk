a60f9b6cbc083707bb8b1977c07bedfe4007e27b
Jonathan Harlap <jharlap@gmail.com>
2013-03-06 10:10:27 -0500

    added Android.mk

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, NetworkLocation/src)
+
+LOCAL_JAVA_LIBRARIES := com.android.location.provider
+
+LOCAL_PACKAGE_NAME := FreeNetworkLocation
+LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_PACKAGE)
