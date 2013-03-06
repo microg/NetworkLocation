45894cc94c22ffcee665565cb2dc13c91f632603
Jonathan Harlap <jharlap@gmail.com>
2013-03-06 10:53:48 -0500

    moved Android.mk

--- NetworkLocation/Android.mk
+++ NetworkLocation/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-java-files-under, NetworkLocation/src)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_JAVA_LIBRARIES := com.android.location.provider
 
